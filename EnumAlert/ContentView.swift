78eb2d55438b6e4e26a3e70eed87fdfab825e5bf
Max <maxim.macari.dam@gmail.com>
2021-03-07 20:10:42 +0100

    Initial Commit

---
+++ EnumAlert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnumAlert
+//
+//  Created by Maxim Macari on 7/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnumAlert/EnumAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumAlertApp.swift
+//  EnumAlert
+//
+//  Created by Maxim Macari on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct EnumAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
